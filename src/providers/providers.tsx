646d514588de3934c94944e59ea7e892842e66ea
SecondSocks <159525526+SecondSocks@users.noreply.github.com>
2025-01-05 14:57:28 +0700

    Create storage

---
+++ src/providers/providers.tsx
@@ -0,0 +1,29 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import { persistor, store } from '@/store/store'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false
+		}
+	}
+})
+
+export function Providers({ children }: Readonly<PropsWithChildren>) {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<PersistGate
+					loading={null}
+					persistor={persistor}
+				>
+					{children}
+				</PersistGate>
+			</Provider>
+		</QueryClientProvider>
+	)
+}
---
+++ src/store/store.ts
@@ -0,0 +1,42 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import {
+	FLUSH,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+	REHYDRATE,
+	persistReducer,
+	persistStore
+} from 'redux-persist'
+
+import { userSlice } from './user/user.slice'
+
+const persistConfig = {
+	key: 'amazon-v2',
+	storage: AsyncStorage,
+	whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
+	// cart: cartSlice.reducer,
+	// carousel: carouselSlice.reducer
+	user: userSlice.reducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+			}
+		})
+})
+
+export const persistor = persistStore(store)
+
+export type TypeRootState = ReturnType<typeof rootReducer>
